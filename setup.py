6161f98305f65dd0d017080ae3b464ae958c57ac
Filip Salomonsson <filip.salomonsson@gmail.com>
2018-05-05 13:57:15 +0200

    Modernize setup.py slightly

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
-from distutils.core import setup
-# patch distutils if it can't cope with the "classifiers" or
-# "download_url" keywords
-from sys import version
-if version < '2.2.3':
-    from distutils.dist import DistributionMetadata
-    DistributionMetadata.classifiers = None
-    DistributionMetadata.download_url = None
+from setuptools import setup, find_packages
 
 import surblclient
 
@@ -16,6 +9,6 @@ setup(name="surblclient",
       long_description=surblclient.__doc__,
       author="Filip Salomonsson",
       author_email="filip.salomonsson@gmail.com",
-      url="http://github.com/infixfilip/surblclient/tree/master",
+      url="http://github.com/filipsalomonsson/surblclient",
       py_modules=["surblclient"],
       )
